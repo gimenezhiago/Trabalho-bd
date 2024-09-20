bf39cff598a3da35474ce612255e2840b1b07aa5
Hiago Gimenez <163415252+gimenezhiago@users.noreply.github.com>
2024-09-20 17:01:31 -0300

    jj

--- Dump-20-09.sql
+++ Dump-20-09.sql
@@ -24,6 +24,7 @@ DROP TABLE IF EXISTS `cargo`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cargo` (
   `idCargo` int unsigned NOT NULL,
+  `nomeCargo` varchar(100) NOT NULL,
   PRIMARY KEY (`idCargo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -69,6 +70,9 @@ DROP TABLE IF EXISTS `fornecedores`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `fornecedores` (
   `idFornecedores` varchar(100) NOT NULL,
+  `nomeFornecedor` varchar(100) NOT NULL,
+  `produtoFornecidos` varchar(100) NOT NULL,
+  `telefone` varchar(15) NOT NULL,
   PRIMARY KEY (`idFornecedores`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -92,10 +96,15 @@ DROP TABLE IF EXISTS `funcionario`;
 CREATE TABLE `funcionario` (
   `cpfPessoa` varchar(11) NOT NULL,
   `idCargo` int unsigned NOT NULL,
+  `dataContratacao` date NOT NULL,
+  `salario` int unsigned NOT NULL,
+  `idStatusFuncionario` int unsigned NOT NULL,
   UNIQUE KEY `funcionario_unique` (`cpfPessoa`),
   KEY `funcionario_cargo_FK` (`idCargo`),
+  KEY `funcionario_statusfuncionario_FK` (`idStatusFuncionario`),
   CONSTRAINT `funcionario_cargo_FK` FOREIGN KEY (`idCargo`) REFERENCES `cargo` (`idCargo`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `funcionario_pessoa_FK` FOREIGN KEY (`cpfPessoa`) REFERENCES `pessoa` (`cpfPessoa`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `funcionario_pessoa_FK` FOREIGN KEY (`cpfPessoa`) REFERENCES `pessoa` (`cpfPessoa`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `funcionario_statusfuncionario_FK` FOREIGN KEY (`idStatusFuncionario`) REFERENCES `statusfuncionario` (`idStatusFuncionario`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -106,6 +115,29 @@ CREATE TABLE `funcionario` (
 LOCK TABLES `funcionario` WRITE;
 /*!40000 ALTER TABLE `funcionario` DISABLE KEYS */;
 /*!40000 ALTER TABLE `funcionario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pagamento`
+--
+
+DROP TABLE IF EXISTS `pagamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pagamento` (
+  `idVenda` int unsigned NOT NULL,
+  UNIQUE KEY `pagamento_unique` (`idVenda`),
+  CONSTRAINT `pagamento_venda_FK` FOREIGN KEY (`idVenda`) REFERENCES `venda` (`idVenda`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pagamento`
+--
+
+LOCK TABLES `pagamento` WRITE;
+/*!40000 ALTER TABLE `pagamento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pagamento` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -133,6 +165,85 @@ LOCK TABLES `pessoa` WRITE;
 /*!40000 ALTER TABLE `pessoa` DISABLE KEYS */;
 /*!40000 ALTER TABLE `pessoa` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `produto`
+--
+
+DROP TABLE IF EXISTS `produto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produto` (
+  `idProduto` int unsigned NOT NULL,
+  `nomeProduto` varchar(100) NOT NULL,
+  `precoUnitario` int unsigned NOT NULL,
+  `quantidadeEstoque` int unsigned NOT NULL,
+  `idFornecedores` varchar(100) NOT NULL,
+  PRIMARY KEY (`idProduto`),
+  KEY `produto_fornecedores_FK` (`idFornecedores`),
+  CONSTRAINT `produto_fornecedores_FK` FOREIGN KEY (`idFornecedores`) REFERENCES `fornecedores` (`idFornecedores`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produto`
+--
+
+LOCK TABLES `produto` WRITE;
+/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `statusfuncionario`
+--
+
+DROP TABLE IF EXISTS `statusfuncionario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `statusfuncionario` (
+  `idStatusFuncionario` int unsigned NOT NULL,
+  `nomeStatusFuncionario` varchar(100) NOT NULL,
+  PRIMARY KEY (`idStatusFuncionario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `statusfuncionario`
+--
+
+LOCK TABLES `statusfuncionario` WRITE;
+/*!40000 ALTER TABLE `statusfuncionario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `statusfuncionario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `venda`
+--
+
+DROP TABLE IF EXISTS `venda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `venda` (
+  `idVenda` int unsigned NOT NULL,
+  `idProduto` int unsigned NOT NULL,
+  `cpfPessoa` varchar(11) NOT NULL,
+  PRIMARY KEY (`idVenda`),
+  KEY `venda_produto_FK` (`idProduto`),
+  KEY `venda_funcionario_FK` (`cpfPessoa`),
+  CONSTRAINT `venda_funcionario_FK` FOREIGN KEY (`cpfPessoa`) REFERENCES `funcionario` (`cpfPessoa`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `venda_produto_FK` FOREIGN KEY (`idProduto`) REFERENCES `produto` (`idProduto`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `venda`
+--
+
+LOCK TABLES `venda` WRITE;
+/*!40000 ALTER TABLE `venda` DISABLE KEYS */;
+/*!40000 ALTER TABLE `venda` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -143,4 +254,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-20 13:29:12
+-- Dump completed on 2024-09-20 16:59:27
